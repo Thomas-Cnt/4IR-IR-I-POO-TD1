57ab0d05301ad1c22e6938ccac2699756305e7d7
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-01 10:30:04 +0100

    -- List one contact without phone number

--- src/main/java/com/thomascantie/insa/ContactsManager.java
+++ src/main/java/com/thomascantie/insa/ContactsManager.java
@@ -11,8 +11,10 @@ public class ContactsManager {
     public void printAllContacts() {
 		if (this.theContact.email == null && this.theContact.phoneNumber == null)
 			System.out.println(this.theContact.name);
-		else
+		else if (this.theContact.email == null)
 			System.out.println(this.theContact.name + ", " + this.theContact.phoneNumber);
+		else
+			System.out.println(this.theContact.name + ", " + this.theContact.email);
     }
 
     public void searchContactByName(String name) {
