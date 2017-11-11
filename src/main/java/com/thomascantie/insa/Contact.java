1961f139269e36dc6d79ac23b5909aff81f05859
Thomas <thomascantie1.0@gmail.com>
2017-11-11 14:44:54 +0100

    -- Refactor with Contact.java

---
+++ src/main/java/com/thomascantie/insa/Contact.java
@@ -0,0 +1,48 @@
+package com.thomascantie.insa;
+
+import java.util.StringJoiner;
+
+class Contact {
+	
+	private String name;
+	private String email;
+	private String phoneNumber;
+	
+	public Contact(String name, String email, String phoneNumber) {
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public boolean hasNameLike(String str) {
+		return this.name.toLowerCase().contains(str.toLowerCase());
+	}
+	
+	@Override
+	public String toString() {
+		return ContactStringBuilder.aContactString()
+				.with(this.name, this.email, this.phoneNumber)
+				.build();
+	}
+	
+}
+
+class ContactStringBuilder {
+	
+	private final StringJoiner joiner = new StringJoiner(", ");
+	
+	public static ContactStringBuilder aContactString() {
+		return new ContactStringBuilder();
+	}
+	
+	public ContactStringBuilder with(String ... data) {
+		for (String str : data) {
+			if (str != null)
+				this.joiner.add(str);
+		}
+		return this;
+	}
+	public String build() {
+		return this.joiner.toString();
+	}
+}
--- src/main/java/com/thomascantie/insa/ContactsManager.java
+++ src/main/java/com/thomascantie/insa/ContactsManager.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactsManager {
-
+	
 	private List<Contact> contacts = new ArrayList<Contact>();
-
+	
 	public void addContact(String name, String email, String phoneNumber) throws InvalidContactNameException, InvalidEmailException {
 		if (name == null || name.isEmpty())
 			throw new InvalidContactNameException();
@@ -14,12 +14,12 @@ public class ContactsManager {
 			throw new InvalidEmailException();
 		this.contacts.add(new Contact(name, email, phoneNumber));
 	}
-
+	
 	public void printAllContacts() {
 		for (Contact aContact : this.contacts)
 			System.out.println(aContact);
 	}
-
+	
 	public void searchContactByName(String name) {
 		int i;
 		for (i = 0; isIndexInTheList(i) && isNotIndexForAContactWithName(name, i); i++) {
@@ -27,33 +27,14 @@ public class ContactsManager {
 		if (isIndexInTheList(i))
 			System.out.println(this.contacts.get(i));
 	}
-
+	
 	private boolean isNotIndexForAContactWithName(String name, int i) {
-		return !this.contacts.get(i).name.toLowerCase().contains(name.toLowerCase());
+		return !this.contacts.get(i).hasNameLike(name);
 	}
-
+	
 	private boolean isIndexInTheList(int i) {
 		return i < this.contacts.size();
 	}
-
-	private class Contact {
-
-		private String name;
-		private String email;
-		private String phoneNumber;
-
-		public Contact(String name, String email, String phoneNumber) {
-			this.name = name;
-			this.email = email;
-			this.phoneNumber = phoneNumber;
-		}
-
-		@Override
-		public String toString() {
-			return ContactStringBuilder.aContactString()
-					.with(this.name, this.email, this.phoneNumber)
-					.build();
-		}
-	}
+	
 }
 
