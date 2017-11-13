34b77409ef659bc2c9725404be19f8a22901457e
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-13 20:44:18 +0100

    -- Add exceptions message

--- src/main/java/com/thomascantie/insa/InvalidContactNameException.java
+++ src/main/java/com/thomascantie/insa/InvalidContactNameException.java
@@ -1,4 +1,8 @@
 package com.thomascantie.insa;
 
 public class InvalidContactNameException extends Exception {
+
+	public InvalidContactNameException(String name) {
+		super("Invalid contact name : \"" + name + "\"");
+	}
 }
--- src/main/java/com/thomascantie/insa/InvalidEmailException.java
+++ src/main/java/com/thomascantie/insa/InvalidEmailException.java
@@ -1,4 +1,8 @@
 package com.thomascantie.insa;
 
 public class InvalidEmailException extends Exception {
+
+	public InvalidEmailException(String email) {
+		super("Invalid email : \"" + email + "\"");
+	}
 }
